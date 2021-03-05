31ee37025b3b31001d5118a0db11433844fda3e8
kkkmail <kkk@jfcglobal.net>
2021-03-05 08:37:52 -0500

    20210305

--- OdePackInterop/Interop.cs
+++ OdePackInterop/Interop.cs
@@ -4,9 +4,58 @@ using System.Runtime.InteropServices;
 
 namespace OdePackInterop
 {
+    /// <summary>
+    /// https://www.codeproject.com/Articles/1099942/FORTRAN-Interoperability-with-NET-Exchanging-Compl
+    /// http://xtechnotes.blogspot.com/2008/07/callback-to-c-from-unmanaged-fortran.html
+    /// https://stackoverflow.com/questions/17549123/c-sharp-performance-using-unsafe-pointers-instead-of-intptr-and-marshal
+    /// </summary>
     public static class Interop
     {
         [DllImport("OdePack.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "DUMSUM")]
-        public static extern void DUMSUM(ref double a, ref double b, ref double c);
+        public static unsafe extern void DUMSUM(ref double a, ref double b, ref double c);
+
+        //[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        //public delegate void F(
+        //    ref int neq,
+        //    ref double t,
+        //    [In, Out] double[] y,
+        //    [In, Out] double[] ydot);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public unsafe delegate void F(
+            ref int neq,
+            ref double t,
+            double* y,
+            double* ydot);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public unsafe delegate void JAC(
+            ref int neq,
+            ref double t,
+            [In, Out] double[] y,
+            ref int ml,
+            ref int mu,
+            [In, Out] double[][] pd,
+            ref int nrowpd);
+
+        [DllImport("OdePack.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "DLSODE")]
+        public static unsafe extern void DLSODE(
+            F f,
+            ref int neq,
+            [In, Out] double[] y,
+            ref double t,
+            ref double tout,
+            ref int itol,
+            [In, Out] double[] rtol,
+            [In, Out] double[] atol,
+            ref int itask,
+            ref int istate,
+            ref int iopt,
+            [In, Out] double[] rwork,
+            ref int lrw,
+            [In, Out] int[] iwork,
+            ref int liw,
+            JAC jac,
+            ref int mf);
     }
 }
--- OdePackInteropTest/Program.cs
+++ OdePackInteropTest/Program.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using static OdePackInterop.Interop;
 
 namespace OdePackInteropTest
 {
     class Program
     {
+        const double fwdCoeff = 0.1;
+        const double bkwCoeff = 0.1;
+
+        //static void FImpl(
+        //    ref int neq,
+        //    ref double t,
+        //    [In, Out] double[] y,
+        //    [In, Out] double[] ydot)
+        //{
+        //    ydot[0] = -fwdCoeff * y[0] + bkwCoeff * ydot[1] * ydot[2];
+        //    ydot[1] = fwdCoeff * y[0] - bkwCoeff * ydot[1] * ydot[2];
+        //    ydot[2] = fwdCoeff * y[0] - bkwCoeff * ydot[1] * ydot[2];
+        //}
+
+        static unsafe void FImpl(
+            ref int neq,
+            ref double t,
+            double* y,
+            double* ydot)
+        {
+            ydot[0] = -fwdCoeff * y[0] + bkwCoeff * y[1] * y[2];
+            ydot[1] = fwdCoeff * y[0] - bkwCoeff * y[1] * y[2];
+            ydot[2] = fwdCoeff * y[0] - bkwCoeff * y[1] * y[2];
+        }
+
+        static unsafe void JacImpl(
+            ref int neq,
+            ref double t,
+            [In, Out] double[] y,
+            ref int ml,
+            ref int mu,
+            [In, Out] double[][] pd,
+            ref int nrowpd)
+        {
+            throw new NotSupportedException("Using Jacobian is not supoprted yet.");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -15,7 +53,75 @@ namespace OdePackInteropTest
             DUMSUM(ref a, ref b, ref c);
 
             Console.WriteLine($"a = {a}, b = {b}, c = {c}.");
-            Console.ReadLine();
+
+            Console.WriteLine("Calling DLSODE...");
+
+            unsafe
+            {
+                F f = FImpl;
+                JAC jac = JacImpl;
+
+                var neq = 3;
+
+                var y = new double[3];
+                y[0] = 1.0;
+                y[1] = 0.2;
+                y[2] = 0.1;
+
+                var t = 0.0;
+                var tout = 10.0;
+                var itol = 2;
+
+                var rtol = new double[3];
+                rtol[0] = 1.0e-4;
+                rtol[1] = 1.0e-4;
+                rtol[2] = 1.0e-4;
+
+                var atol = new double[3];
+                atol[0] = 1.0e-6;
+                atol[1] = 1.0e-6;
+                atol[2] = 1.0e-6;
+
+                var itask = 1;
+                var istate = 1;
+                var iopt = 0;
+                var mf = 23;
+                var lrw = 100;
+                var liw = 100;
+
+                var iwork = new int[liw];
+                var rwork = new double[lrw];
+
+                // Pin everything so that GC won't move the things around...
+                fixed (double* _ = &y[0], __ = &rtol[0], ___ = &atol[0], ____ = &rwork[0])
+                {
+                    fixed (int* _____ = &iwork[0])
+                    {
+                        DLSODE(
+                            f,
+                            ref neq,
+                            y,
+                            ref t,
+                            ref tout,
+                            ref itol,
+                            rtol,
+                            atol,
+                            ref itask,
+                            ref istate,
+                            ref iopt,
+                            rwork,
+                            ref lrw,
+                            iwork,
+                            ref liw,
+                            jac,
+                            ref mf);
+                    }
+                }
+
+                Console.WriteLine($"At t = {t}, y[0] = {y[0]}, y[1] = {y[1]}, y[2] = {y[2]}");
+                Console.WriteLine($"No. steps = {iwork[10]}, No. f-s = {iwork[11]}, No. J-s = {iwork[12]}");
+                Console.ReadLine();
+            }
         }
     }
 }
