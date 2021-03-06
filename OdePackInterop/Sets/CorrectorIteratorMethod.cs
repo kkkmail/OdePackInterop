f7ecdccc5b866e86ac3b0b26e5217e91cfd0fc63
kkkmail <kkk@jfcglobal.net>
2021-03-05 19:13:41 -0500

    20210305

---
+++ OdePackInterop/Sets/CorrectorIteratorMethod.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OdePackInterop.Sets
+{
+    public record CorrectorIteratorMethod : ClosedDualSetBase<CorrectorIteratorMethod, int, string>
+    {
+        /// <summary>
+        /// If true then either user supplied full or banded Jacobian is required OR it will be calculated.
+        /// In a sense this is a measure of how many calls to F per step will be made.
+        /// </summary>
+        public bool UseJacobian { get; }
+
+        public bool RequireUserJacobian { get; }
+
+        private CorrectorIteratorMethod(
+            int key,
+            [CallerMemberName] string? value = null,
+            bool useJacobian = false,
+            bool requireUserJacobian = false) : base(key, value!)
+        {
+            UseJacobian = useJacobian;
+            RequireUserJacobian = requireUserJacobian;
+        }
+
+        public static CorrectorIteratorMethod Functional { get; } = new(0);
+        public static CorrectorIteratorMethod ChordWithUserJacobian { get; } = new(1, useJacobian: true, requireUserJacobian: true);
+        public static CorrectorIteratorMethod ChordWithGeneratedJacobian { get; } = new(2, useJacobian: true);
+        public static CorrectorIteratorMethod ChordWithDiagonalJacobian { get; } = new(3);
+        public static CorrectorIteratorMethod ChordWithBandedUserJacobian { get; } = new(4, useJacobian: true, requireUserJacobian: true);
+        public static CorrectorIteratorMethod ChordWithBandedGeneratedJacobian { get; } = new(5, useJacobian: true);
+    }
+}
---
+++ OdePackInterop/Sets/MethodFlag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdePackInterop.Sets
+{
+    public record MethodFlag : ClosedDualSetBase<MethodFlag, int, string>
+    {
+        private MethodFlag(int key, [CallerMemberName] string? value = null) : base(key, value!)
+        {
+        }
+
+        public static MethodFlag Adams { get; } = new(1);
+        public static MethodFlag Bdf { get; } = new(2);
+    }
+}
