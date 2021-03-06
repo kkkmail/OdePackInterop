233598990c6af4a5707ad32cdc8511c9aaec5b53
kkkmail <kkk@jfcglobal.net>
2021-03-05 19:59:29 -0500

    20210305

--- OdePackInterop/Sets/CorrectorIteratorMethod.cs
+++ OdePackInterop/Sets/CorrectorIteratorMethod.cs
@@ -10,6 +10,9 @@ namespace OdePackInterop.Sets
         /// </summary>
         public bool UseJacobian { get; }
 
+        /// <summary>
+        /// User must supply a Jacobian function.
+        /// </summary>
         public bool RequireUserJacobian { get; }
 
         private CorrectorIteratorMethod(
@@ -18,15 +21,15 @@ namespace OdePackInterop.Sets
             bool useJacobian = false,
             bool requireUserJacobian = false) : base(key, value!)
         {
-            UseJacobian = useJacobian;
             RequireUserJacobian = requireUserJacobian;
+            UseJacobian = useJacobian || requireUserJacobian;
         }
 
         public static CorrectorIteratorMethod Functional { get; } = new(0);
-        public static CorrectorIteratorMethod ChordWithUserJacobian { get; } = new(1, useJacobian: true, requireUserJacobian: true);
+        public static CorrectorIteratorMethod ChordWithUserJacobian { get; } = new(1, requireUserJacobian: true);
         public static CorrectorIteratorMethod ChordWithGeneratedJacobian { get; } = new(2, useJacobian: true);
         public static CorrectorIteratorMethod ChordWithDiagonalJacobian { get; } = new(3);
-        public static CorrectorIteratorMethod ChordWithBandedUserJacobian { get; } = new(4, useJacobian: true, requireUserJacobian: true);
+        public static CorrectorIteratorMethod ChordWithBandedUserJacobian { get; } = new(4, requireUserJacobian: true);
         public static CorrectorIteratorMethod ChordWithBandedGeneratedJacobian { get; } = new(5, useJacobian: true);
     }
 }
